e135aa67dbee19ca1c89d896815a6819e3271aa4
Eugene Sobol <esobol@insoftgroup.com>
2017-10-20 13:20:18 +0300

    Initial Commit

---
+++ Railway/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Railway
+//
+//  Created by Соболь Евгений on 10/20/17.
+//  Copyright © 2017 Insoft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Railway/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Railway
+//
+//  Created by Соболь Евгений on 10/20/17.
+//  Copyright © 2017 Insoft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
